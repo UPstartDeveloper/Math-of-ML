b69a10c54254aee9d1d88d81e280fd34ccd056a7
= <zainr7989@gmail.com>
2022-10-12 21:49:28 -0400

    Question standardization code

--- HW-2/Movie_RecSys_SVD_Revised.py
+++ HW-2/Movie_RecSys_SVD_Revised.py
@@ -15,7 +15,7 @@
 
 # **Download the dataset from here:** https://grouplens.org/datasets/movielens/1m/
 
-# In[177]:
+# In[1]:
 
 
 # Import all the required libraries
@@ -26,7 +26,7 @@ import pandas as pd
 # ## Reading the Data
 # Now that we have downloaded the files from the link above and placed them in the same directory as this Jupyter Notebook, we can load each of the tables of data as a CSV into Pandas. Execute the following, provided code.
 
-# In[178]:
+# In[2]:
 
 
 # Read the dataset from the two files into ratings_data and movies_data
@@ -34,14 +34,14 @@ column_list_ratings = ["UserID", "MovieID", "Ratings","Timestamp"]
 ratings_data  = pd.read_csv('ratings.dat',sep='::',names = column_list_ratings, engine="python")
 
 
-# In[179]:
+# In[3]:
 
 
 print(ratings_data.shape)
 ratings_data.head()
 
 
-# In[180]:
+# In[4]:
 
 
 column_list_movies = ["MovieID","Title","Genres"]
@@ -49,20 +49,20 @@ column_list_movies = ["MovieID","Title","Genres"]
 movies_data = pd.read_csv('movies.dat',sep = '::',names = column_list_movies, engine="python", encoding='unicode_escape')
 
 
-# In[181]:
+# In[5]:
 
 
 print(movies_data.shape)
 movies_data.head()
 
 
-# In[182]:
+# In[6]:
 
 
 movies_data["Title"].nunique()  # just curious
 
 
-# In[183]:
+# In[7]:
 
 
 a = movies_data["MovieID"].unique() # just curious
@@ -72,21 +72,21 @@ a[:70]
 
 # `ratings_data`, `movies_data`, `user_data` corresponds to the data loaded from `ratings.dat`, `movies.dat`, and `users.dat` in Pandas.
 
-# In[184]:
+# In[8]:
 
 
 column_list_users = ["UserID","Gender","Age","Occupation","Zixp-code"]
 user_data = pd.read_csv("users.dat",sep = "::",names = column_list_users, engine="python")
 
 
-# In[185]:
+# In[9]:
 
 
 print(user_data.shape)
 user_data.head()
 
 
-# In[186]:
+# In[10]:
 
 
 user_data["UserID"].nunique() # just curious to see if there are duplicates
@@ -96,7 +96,7 @@ user_data["UserID"].nunique() # just curious to see if there are duplicates
 
 # We now have all our data in Pandas - however, it's as three separate datasets! To make some more sense out of the data we have, we can use the Pandas `merge` function to combine our component data-frames. Run the following code:
 
-# In[187]:
+# In[11]:
 
 
 data=pd.merge(pd.merge(ratings_data,user_data),movies_data)
@@ -106,7 +106,7 @@ data.head()
 
 # Next, we can create a pivot table to match the ratings with a given movie title. Using `data.pivot_table`, we can aggregate (using the average/`mean` function) the reviews and find the average rating for each movie. We can save this pivot table into the `mean_ratings` variable. 
 
-# In[188]:
+# In[12]:
 
 
 mean_ratings=data.pivot_table('Ratings','Title',aggfunc='mean')
@@ -115,7 +115,7 @@ mean_ratings
 
 # Now, we can take the `mean_ratings` and sort it by the value of the rating itself. Using this and the `head` function, we can display the top 15 movies by average rating.
 
-# In[189]:
+# In[13]:
 
 
 mean_ratings=data.pivot_table('Ratings',index=["Title"],aggfunc='mean')
@@ -125,7 +125,7 @@ top_15_mean_ratings
 
 # Let's adjust our original `mean_ratings` function to account for the differences in gender between reviews. This will be similar to the same code as before, except now we will provide an additional `columns` parameter which will separate the average ratings for men and women, respectively.
 
-# In[190]:
+# In[14]:
 
 
 mean_ratings=data.pivot_table('Ratings',index=["Title"],columns=["Gender"],aggfunc='mean')
@@ -134,7 +134,7 @@ mean_ratings
 
 # We can now sort the ratings as before, but instead of by `Rating`, but by the `F` and `M` gendered rating columns. Print the top rated movies by male and female reviews, respectively.
 
-# In[191]:
+# In[15]:
 
 
 data=pd.merge(pd.merge(ratings_data,user_data),movies_data)
@@ -144,7 +144,7 @@ top_female_ratings = mean_ratings.sort_values(by='F', ascending=False)
 print(top_female_ratings.head(15))
 
 
-# In[192]:
+# In[16]:
 
 
 top_male_ratings = mean_ratings.sort_values(by='M', ascending=False)
@@ -153,7 +153,7 @@ print(top_male_ratings.head(15))
 
 # Zain: the next cell adds an additional column, just to see the difference between the avg rating given by men and women for a given film?
 
-# In[193]:
+# In[17]:
 
 
 mean_ratings['diff'] = mean_ratings['M'] - mean_ratings['F']
@@ -163,7 +163,7 @@ sorted_by_diff[:10]
 
 # Let's try grouping the data-frame, instead, to see how different titles compare in terms of the number of ratings. Group by `Title` and then take the top 10 items by number of reviews. We can see here the most popularly-reviewed titles.
 
-# In[194]:
+# In[18]:
 
 
 ratings_by_title=data.groupby('Title').size()
@@ -172,13 +172,13 @@ ratings_by_title.sort_values(ascending=False).head(10)
 
 # Similarly, we can filter our grouped data-frame to get all titles with a certain number of reviews. Filter the dataset to get all movie titles such that the number of reviews is >= 2500.
 
-# In[195]:
+# In[19]:
 
 
 ratings_by_title_over_2500 = ratings_by_title[ratings_by_title > 2500]
 
 
-# In[196]:
+# In[20]:
 
 
 # sanity check
@@ -196,13 +196,13 @@ if ratings_by_title_over_2500.min()  > 2500:
 # - If you're stuck, you might want to look into the `np.ndarray` datatype and how to create one of the desired shape.
 # - Every review lies between 1 and 5, and thus fits within a `uint8` datatype, which you can specify to numpy.
 
-# In[197]:
+# In[21]:
 
 
 from collections import OrderedDict
 
 
-# In[198]:
+# In[22]:
 
 
 # Create the matrix
@@ -227,11 +227,18 @@ for index_user, uid in enumerate(unique_user_ids):
     ratings[index_user][:] = list(user_ratings_per_movie_sparse.values())
 
 
-# In[199]:
+# In[23]:
 
 
 # Print the shape - expected shape is [6040, 3883]?
 print(ratings.shape)
+
+
+# In[25]:
+
+
+# sanity check - no np.nan values, yes?
+np.isnan(ratings).sum()
 
 
 # ## Question 2
@@ -246,7 +253,7 @@ print(ratings.shape)
 # 
 # - While creating the ratings matrix, you might not get any NaN values but if you look closely, you will see 0 values. This should be treated as NaN values as the original data does not have 0 rating. This should be replaced by the average.
 
-# In[200]:
+# In[31]:
 
 
 # 1: which axis to get the mean? ---> target shape is (1, 3883) --> axis 0
@@ -255,33 +262,42 @@ print(avg_rating_per_movie.shape)
 np.isnan(avg_rating_per_movie).sum()
 
 
-# In[201]:
+# In[27]:
 
 
-#2: replacing NaN
+#2: replacing NaN (aka 0)
 ratings_no_nan = np.copy(ratings)
 for movie_index in range(ratings.shape[1]):
     movie_ratings = ratings[:][movie_index]
-    movie_ratings_no_nan = np.nan_to_num(movie_ratings, nan=avg_rating_per_movie[movie_index])
+    movie_ratings_no_nan = np.where(movie_ratings == 0, avg_rating_per_movie[movie_index], movie_ratings)
+    # np.nan_to_num(movie_ratings, nan=avg_rating_per_movie[movie_index])
     ratings_no_nan[:][movie_index] = movie_ratings_no_nan
 
 
-# Quick sanity check: are there any remaining `NaN` values?
+# Quick sanity check: are there any remaining `NaN` values (aka zero)?
 
-# In[202]:
-
-
-np.isnan(ratings_no_nan).sum()
+# In[29]:
 
 
-# In[203]:
+(ratings_no_nan == 0).sum()
+
+
+# Hmm, that's weird. I believe this might not be easily fixed, as some movies might just have `0` as their rating. We can verify:
+
+# In[33]:
+
+
+(avg_rating_per_movie == 0).sum()
+
+
+# In[34]:
 
 
 stddev = np.std(ratings_no_nan, axis=0).shape
 (np.isnan(stddev)).sum()
 
 
-# In[204]:
+# In[35]:
 
 
 # 2: standardization
@@ -294,7 +310,7 @@ np.isnan(ratings_normalized).sum()
 
 # We're now going to perform Singular Value Decomposition (SVD) on the normalized ratings matrix from the previous question. Perform the process using numpy, and along the way print the shapes of the $U$, $S$, and $V$ matrices you calculated.
 
-# In[205]:
+# In[36]:
 
 
 # Compute the SVD of the normalised matrix
@@ -302,7 +318,7 @@ U, S, V = np.linalg.svd(ratings_normalized)
 S = np.diag(S)
 
 
-# In[206]:
+# In[37]:
 
 
 # Print the shapes
@@ -313,7 +329,7 @@ print(U.shape, S.shape, V.shape)
 
 # Reconstruct four rank-k rating matrix $R_k$, where $R_k = U_kS_kV_k^T$ for k = [100, 1000, 2000, 3000]. Using each of $R_k$ make predictions for 3 users (select them from the dataset) for the movie with ID 1377 (Batman Returns).
 
-# In[207]:
+# In[38]:
 
 
 # which col did we put Batman Returns in? -->
@@ -340,7 +356,7 @@ for k in [100, 1_000, 2_000, 3_000]:
 
 # **Based on the reconstruction rank-1000 rating matrix $R_{1000}$ and the cosine similarity,** sort the movies which are most similar. You will have a function `top_cosine_similarity` which sorts data by its similarity to a movie with ID `movie_id` and returns the top $n$ items, and a second function `print_similar_movies` which prints the titles of said similar movies. Return the top 5 movies for the movie with ID `1377` (*Batman Returns*):
 
-# In[208]:
+# In[39]:
 
 
 # Sort the movies based on cosine similarity
@@ -372,4 +388,10 @@ movie_id = 1377
 rank = 1000
 data = U[:, :rank] @ S[:rank, :rank] @ V[:rank, :]
 print_similar_movies(data, 1377, 5)
+
+
+# In[ ]:
+
+
+
 
